2b4bfe9ebe16fb240c0940af86695532cd2e969d
Aker <88575418+KemononLine@users.noreply.github.com>
2024-11-24 21:14:06 -0500

    Add files via upload
    Test model with stablebaseline 3

---
+++ test_takeoff_ppo_sb3.py
@@ -0,0 +1,49 @@
+import os
+import gymnasium as gym
+from stable_baselines3 import PPO
+from gym_pybullet_drones.envs.TakeoffAviary import TakeoffAviary
+from gym_pybullet_drones.utils.enums import ObservationType, ActionType
+import time
+
+def test_takeoff():
+    model_path = "sb3_takeoff_logs/ppo_takeoff_final.zip" 
+    if not os.path.exists(model_path):
+        print("Model file not found! Please check the path.")
+        return
+
+
+    model = PPO.load(model_path)
+
+    env = TakeoffAviary(
+        obs=ObservationType("kin"),
+        act=ActionType("rpm")
+    )
+
+
+    obs, info = env.reset(seed=42)
+
+    for episode in range(5):
+
+        obs, info = env.reset(seed=42)
+        done = False
+        total_reward = 0
+
+        while not done:
+
+            action, _states = model.predict(obs, deterministic=True)
+
+
+            obs, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+
+            env.render()
+            time.sleep(0.5)
+
+            done = terminated or truncated
+
+        print(f"Episode {episode + 1} finished with total reward: {total_reward}")
+
+    env.close()
+
+if __name__ == "__main__":
+    test_takeoff()
